365a76be7789fe7a975d025cfd64e640b9bccff3
Nithish-1311 <gvbot999@gmail.com>
2026-04-17 08:45:15 +0530

    Added getters and setters manully

--- src/main/java/com/gemini/BusTicketBookingSystem/entity/Route.java
+++ src/main/java/com/gemini/BusTicketBookingSystem/entity/Route.java
@@ -6,7 +6,7 @@ import lombok.*;
 
 @Entity
 @Table(name = "routes")
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+ @Builder
 public class Route {
 
     @Id
@@ -27,4 +27,55 @@ public class Route {
 
     @Column(name = "duration")
     private Integer duration;
+
+    public Integer getRouteId() {
+        return routeId;
+    }
+
+    public void setRouteId(Integer routeId) {
+        this.routeId = routeId;
+    }
+
+    public String getFromCity() {
+        return fromCity;
+    }
+
+    public void setFromCity(String fromCity) {
+        this.fromCity = fromCity;
+    }
+
+    public String getToCity() {
+        return toCity;
+    }
+
+    public void setToCity(String toCity) {
+        this.toCity = toCity;
+    }
+
+    public Integer getBreakPoints() {
+        return breakPoints;
+    }
+
+    public void setBreakPoints(Integer breakPoints) {
+        this.breakPoints = breakPoints;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public Route() {
+    }
+
+    public Route(Integer routeId, String fromCity, String toCity, Integer breakPoints, Integer duration) {
+        this.routeId = routeId;
+        this.fromCity = fromCity;
+        this.toCity = toCity;
+        this.breakPoints = breakPoints;
+        this.duration = duration;
+    }
 }
